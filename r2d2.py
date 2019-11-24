f7337256714d751183cca8d351d42f2fb8398ccd
Espen Trydal <espen@fastmail.com>
2019-11-24 14:28:25 +0100

    added remote on off by bluetooth bluedot

--- r2d2.py
+++ r2d2.py
@@ -6,33 +6,54 @@
 from move import Move
 from RPi.GPIO import cleanup
 import traceback
+from bluedot import BlueDot
+from signal import pause
+from r2d2_defines import MOV_STOP
 
-if __name__ == 'r2d2':
-    m = Move()
-    m.move_set_speed(m.MIN_SPEED)
-    print("Ready!")
+class R2D2:
+    button_pressed = 0
+    
+    @classmethod
+    def toggle_press(cls, value):
+        cls.button_pressed = value
+
+    def __init__(self):
+        self.m = Move()
+        self.bd = BlueDot()
+
+        self.m.move_set_speed(self.m.MIN_SPEED)
+
+    def start(self):
+        try:
+            self.m.move_forward()
+            print("move_speed:", self.m.move_speed)
+            self.m.roam()
+                
+
+        except KeyboardInterrupt:   # Reset by pressing CTRL + C
+            print("Error:", traceback.format_exc())
+            print("Stopping motors...:")
+            self.m.move_stop()
+            cleanup()
+
+        except Exception:
+            print("Error:", traceback.format_exc())
+            print("Stopping motors...:")
+            self.m.move_stop()
+            cleanup()
+
+    def stop(self):
+        print("Bluedot stopping motors...:")
+        self.m.move_stop()
+        cleanup()
 
 if __name__ == '__main__':
-    m = Move()
-    m.move_set_speed(m.MIN_SPEED)
+    r = R2D2()
     print("Ready!")
+    
+    r.bd.when_pressed = r.start
+    r.bd.when_moved = r.start
+    r.bd.when_released = r.stop
+    pause()
 
-    try:
-        while True:
-            m.move_forward()
-            print("move_speed:", m.move_speed)
-            m.roam()
-
-    except KeyboardInterrupt:   # Reset by pressing CTRL + C
-        print("Error:", traceback.format_exc())
-        print("Stopping motors...:")
-        m.move_stop()
-        cleanup()
-
-    except Exception:
-        print("Error:", traceback.format_exc())
-        print("Stopping motors...:")
-        m.move_stop()
-        cleanup()
-        
 # End of file
