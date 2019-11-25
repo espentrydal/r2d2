c80288a3880a6644edb1120eaf4f2062378659d2
Espen Trydal <espen@fastmail.com>
2019-11-25 15:41:12 +0100

    nunchuk enable button working

--- nunchuk-mbed/src/main.cpp
+++ nunchuk-mbed/src/main.cpp
@@ -6,17 +6,15 @@ DigitalOut myled(PA_5);
 
 int main()
 {
-  int change = -1;
   while (1) {
-    if (change != mybutton) {
-      change = mybutton;
-      if (mybutton == 0) {
-        printf("$BUTTON, 1\r\n");
-      }
-      else {
-        printf("$BUTTON, 0\r\n");
-      }
-      myled = mybutton;
+    if (mybutton == 0) {
+      printf("$BUTTON, 0\r\n");
     }
+    else {
+      printf("$BUTTON, 1\r\n");
+    }
+    myled = mybutton;
+    wait_ms(500);
   }
 }
+
